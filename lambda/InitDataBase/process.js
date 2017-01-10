b514b8be2d7330d308d3b3c2fad2e8ff8df01eb6
John M Calhoun <johnmcalhoun123@gmail.com>
2017-01-09 21:15:08 -0600

    wrote graph table create and destroy scripts

--- lambda/InitDataBase/process.js
+++ lambda/InitDataBase/process.js
@@ -1,32 +1,13 @@
-var aws=require('aws-sdk')
-var region=process.env.REGION
-if(region){
 
-}else{
-    region='us-east-1'
-}
-
-var s3=new aws.S3(process.env.REGION)
+//run script
+    //load in string
+    //create cooonectoin
+    //run
 
 module.exports.create=function(event){   
     var out=new Promise(function(resolve,reject){ 
         if(event.RequestType==="Create" | event.RequestType==="Update"){
-            var props=event.ResourceProperties
-            var params={
-                Bucket:props.bucket,
-                Key:props.key,
-                Body:"Healthy!"
-            }
-            
-            s3.putObject(
-                params,
-                function(err,data){
-                    if(err){
-                        reject(err)
-                    }else{
-                        resolve(data)
-                    }
-                })
+            resolve()
         }else{
             resolve()
         }
@@ -36,24 +17,7 @@ module.exports.create=function(event){
 module.exports.destory=function(event){
     var out=new Promise(function(resolve,reject){
         if(event.RequestType==="Delete" | event.RequestType==="Update"){
-            var props=event.ResourceProperties
-            var params={
-                Bucket:props.bucket,
-                Key:props.key
-            }
-            
-            s3.deleteObject(
-                params,
-                function(err,data){
-                    if(err){
-                        console.log(err)
-                        reject(err)
-                    }else{
-                        resolve(data)
-                    }
-                }
-                )
-           
+            resolve()
         }else{
             resolve()
         }
---
+++ scripts/destroy.sql
@@ -0,0 +1,11 @@
+--Create nessasery tables and users
+USE ?;
+
+DROP TABLE edgelist;
+DROP TABLE edgeattributes;
+DROP TABLE nodelist;
+DROP TABLE edgeAttributes;
+
+DROP USER 'write'
+DROP USER 'read'
+
